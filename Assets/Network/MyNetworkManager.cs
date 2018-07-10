2fe3d5d8952aeb35ca47a6bddee03f6c156e1ba6
DivineErdrick <40323567+DivineErdrick@users.noreply.github.com>
2018-07-10 16:39:33 -0500

    Host and Client test.

---
+++ Assets/Network/MyNetworkManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MyStartHost () {
+
+        Debug.Log(Time.timeSinceLevelLoad + ": Host attempting to start.");
+        StartHost();
+    }
+
+    public override void OnStartHost () {
+
+        Debug.Log(Time.timeSinceLevelLoad + ": Host started.");
+    }
+
+    public override void OnStartClient (NetworkClient myClient) {
+
+        Debug.Log(Time.timeSinceLevelLoad + ": Client start requested.");
+    }
+
+    public override void OnClientConnect(NetworkConnection myConn) {
+
+        Debug.Log(Time.timeSinceLevelLoad + ": Client has connected to IP: " + myConn.address);
+    }
+}
