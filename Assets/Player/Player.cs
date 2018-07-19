92223b730019f95e6528feda9d517a64588627e0
DivineErdrick <40323567+DivineErdrick@users.noreply.github.com>
2018-07-19 17:27:38 -0500

    GameDev.tv - Ahoy Matey: Network Player Movement part 1

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [Range(0f, 60f)]
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CrossPlatformInputManager.GetAxis("Horizontal") != 0) {
+
+            transform.Translate(Vector3.right * CrossPlatformInputManager.GetAxis("Horizontal") * speed * Time.deltaTime);
+        }		
+	}
+}
